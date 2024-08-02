2c4568af4c5c1337f68d72b87bd563dc70d4cc76
Leodomi <Leodomi15@gmail.com>
2024-08-01 22:36:02 -0400

    agregado el slice y componente de notificacion

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,9 +2,11 @@ import { create } from "zustand";
 import {devtools } from 'zustand/middleware'
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { createFavoriteSlice, FavoriteSliceType } from "./favoriteSlice";
+import { NotificationSliceType, createNotificationSlice } from "./NotificationSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoriteSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoriteSliceType & NotificationSliceType>()(devtools((...a) => ({
     //esto hace que se puedan añadir slices
     ...createRecipesSlice(...a),
     ...createFavoriteSlice(...a),
+    ...createNotificationSlice(...a)
 })))
--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,6 +1,23 @@
+import { useMemo } from "react"
+import { DrinkCard } from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 export const FavoritesPage = () => {
+  const { favorites } = useAppStore()
+  const hasFavorites=useMemo(() => favorites.length > 0, [favorites]);
   return (
-    <h1>FavoritesPage</h1>
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+      {!hasFavorites && <p className="text-2xl my-10 text-center">No hay favoritos aún</p>}
+
+      {hasFavorites && <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 my-10">
+        {favorites.map((drink) => (
+          <DrinkCard key={drink.idDrink} drink={drink} />
+        ))}
+      </div>}
+      
+
+
+    </>
   )
 }
